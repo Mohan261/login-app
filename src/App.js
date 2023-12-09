073cd29f00f88c6376e17f81c259255d659cbf22
Mohan261 <mohan2606201@gmail.com>
2023-12-09 17:48:55 +0530

    first

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/login/home" element={<Home />} />
+        <Route path="/login-app" element={<Login />} />
+        <Route path="/login-app/home" element={<Home />} />
         <Route path="/*" element={<Login />} />
       </Routes>
     </BrowserRouter>
